07b4fb1db286dbe4da0830cd7d006c55652808f3
christianngouw <christiangouw168@gmail.com>
2021-10-16 13:04:01 +0700

    feat: add new color variables and code refactoring:

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pocket/components/header.dart';
+import 'package:flutter_pocket/components/myPlant.dart';
+import 'package:flutter_pocket/components/quickScan.dart';
+import 'package:flutter_pocket/components/taskList.dart';
 import 'package:flutter_pocket/variables/colorVariables.dart';
 import 'package:flutter_pocket/variables/fontVariables.dart';
 
@@ -11,6 +15,7 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: ColorVariables.uiGray,
       body: SafeArea(
         child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
           child: Stack(
             children: [
               Container(
@@ -25,249 +30,10 @@ class HomeScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Container(
-                      child: RichText(
-                        text: TextSpan(
-                          text: 'Hi, ',
-                          style: FontVariables.getStyle(
-                            TextSize.xxl,
-                            ColorVariables.white,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'Mina Honey',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 2),
-                      child: Text(
-                        'Ada 3 lapangan yang harus dipupuk',
-                        style: FontVariables.getStyle(
-                          TextSize.xs,
-                          Colors.white,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 30),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Kerjaan Saya',
-                            style: FontVariables.getStyle(
-                              TextSize.lg,
-                              Colors.white,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                'Lihat Lebih',
-                                style: FontVariables.getStyle(
-                                    TextSize.xs, Colors.white),
-                              ),
-                              Icon(
-                                Icons.chevron_right_rounded,
-                                color: Colors.white,
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      height: 140,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        physics: BouncingScrollPhysics(),
-                        children: [
-                          Container(
-                            width: 280,
-                            margin: EdgeInsets.only(right: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 18,
-                                vertical: 15,
-                              ),
-                              child: Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            'Memberikan Pupuk',
-                                            style: FontVariables.getStyle(
-                                              TextSize.base,
-                                              Colors.black,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 4),
-                                          Text(
-                                            '3 Jam lagi',
-                                            style: FontVariables.getStyle(
-                                              TextSize.sm,
-                                              ColorVariables.riverStone,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      Icon(Icons.more_vert)
-                                    ],
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Divider(
-                                    thickness: 1.2,
-                                    height: 1.2,
-                                    color: ColorVariables.riverStone
-                                        .withOpacity(0.2),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Icon(
-                                        Icons.location_pin,
-                                        color: ColorVariables.primary,
-                                        size: 25,
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.only(left: 5),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              'Kebun Pepaya samping Gudang',
-                                              style: FontVariables.getStyle(
-                                                  TextSize.xs,
-                                                  ColorVariables.riverStone),
-                                            ),
-                                            const SizedBox(height: 3),
-                                            Text(
-                                              '3.2Km',
-                                              style: FontVariables.getStyle(
-                                                  TextSize.xs,
-                                                  ColorVariables.riverStone),
-                                            )
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Container(
-                            width: 280,
-                            margin: EdgeInsets.only(right: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 18,
-                                vertical: 15,
-                              ),
-                              child: Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            'Memberikan Pupuk',
-                                            style: FontVariables.getStyle(
-                                              TextSize.base,
-                                              Colors.black,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 4),
-                                          Text(
-                                            '3 Jam lagi',
-                                            style: FontVariables.getStyle(
-                                              TextSize.sm,
-                                              ColorVariables.riverStone,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      Icon(Icons.more_vert)
-                                    ],
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Divider(
-                                    thickness: 1.2,
-                                    height: 1.2,
-                                    color: ColorVariables.riverStone
-                                        .withOpacity(0.2),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Icon(
-                                        Icons.location_pin,
-                                        color: ColorVariables.primary,
-                                        size: 25,
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.only(left: 5),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              'Kebun Pepaya samping Gudang',
-                                              style: FontVariables.getStyle(
-                                                  TextSize.xs,
-                                                  ColorVariables.riverStone),
-                                            ),
-                                            const SizedBox(height: 3),
-                                            Text(
-                                              '3.2Km',
-                                              style: FontVariables.getStyle(
-                                                  TextSize.xs,
-                                                  ColorVariables.riverStone),
-                                            )
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
+                    Header(),
+                    TaskList(),
+                    QuickScan(),
+                    MyPlant(),
                   ],
                 ),
               ),
--- lib/variables/colorVariables.dart
+++ lib/variables/colorVariables.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class ColorVariables {
   static const primary = Color(0xFF00C875);
-  static const secondary = Color(0xFF00A25B);
+  static const primaryHover = Color(0xFF00A25B);
+  static const secondary = Color(0xFFF5EDA8);
 
   static const uiGray = Color(0xFFF3F4F6);
   static const riverStone = Color(0xFF999999);
